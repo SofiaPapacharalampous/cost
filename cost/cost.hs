e8c76c88e07c8fb3da304f89f73d0658fec11c52
Sofia Papacharalampous <sofia.papacharalampous@gmail.com>
2018-03-29 12:47:18 +0300

    Update cost.hs

--- cost/cost.hs
+++ cost/cost.hs
@@ -1,6 +1,6 @@
 cost:: (Int, Int, Int) -> (Int, Int, Int) -> Float
 
-cost (h1, m1, s1) (h2, m2, s2) {--wra enar3hs kai wra lh3hs--}
+cost (h1, m1, s1) (h2, m2, s2) 
 
 	| ((h2*3600 + m2*60 + s2) - (h1*3600 + m1*60 + s1)) == 0 = 0
 	
